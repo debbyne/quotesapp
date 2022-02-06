9ad787bcf328452244937cb4d54cfc26c1dfeebd
debbyne <kinyadebbyne@gmail.com>
2022-02-07 02:38:20 +0300

    add correction identation

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -4,18 +4,16 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 @Component({
   selector: 'app-form',
   templateUrl: './form.component.html',
-  styleUrls: ['./form.component.css']
+  styleUrls: ['./form.component.css'],
 })
 export class FormComponent implements OnInit {
-  newQuote = new QuoteClass(0,"","","",0,0,new Date());
+  newQuote = new QuoteClass(0, '', '', '', 0, 0, new Date());
   @Output() addQuote = new EventEmitter<QuoteClass>();
   submitQuote() {
     this.addQuote.emit(this.newQuote);
   }
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
--- src/app/quote-class.ts
+++ src/app/quote-class.ts
@@ -1,3 +1,11 @@
 export class QuoteClass {
-    constructor(public id:number,public quote:string,public author:string,public concoctor:string,public upvotes:number,public downvotes:number,public date:Date){}
+  constructor(
+    public id: number,
+    public quote: string,
+    public author: string,
+    public concoctor: string,
+    public upvotes: number,
+    public downvotes: number,
+    public date: Date
+  ) {}
 }
