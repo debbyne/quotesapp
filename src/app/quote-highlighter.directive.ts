7d49b832fe63a26b74be87d42af21672304dce0a
debbyne <kinyadebbyne@gmail.com>
2022-02-07 11:10:33 +0300

    add highlighter

--- src/app/quote-highlighter.directive.ts
+++ src/app/quote-highlighter.directive.ts
@@ -1,10 +1,26 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, HostListener } from '@angular/core';
 
 @Directive({
   selector: '[appQuoteHighlighter]'
 })
 export class QuoteHighlighterDirective {
 
-  constructor() { }
+
+  constructor(private elem: ElementRef) { }
+  private textDeco(action: string) {
+    this.elem.nativeElement.style.color = action;
+  }
+
+  @HostListener("click") onClicks() {
+    this.textDeco("red")
+  }
+
+  @HostListener("dblclick") onDoubleClicks() {
+    this.textDeco("yellow")
+  }
+
+
+
+
 
 }
